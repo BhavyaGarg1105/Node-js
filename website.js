eed14d730c96cbfc3abab6b660480f9f5835f01f
BhavyaGarg1105 <bhavyagarg1105@gmail.com>
2023-09-23 18:28:20 +0530

    Learning about if else in HTTP server.

--- website.js
+++ website.js
@@ -1,22 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.env.port || 3000;
 
 const server = http.Server((req, res)=>{
     console.log(req.url);
     res.setHeader('Content-Type','text/html')
+
     if(req.url=='/'){
         res.statusCode = 200;
         res.end('<h1> Hello this is Bhavya.!! </h1> <p>This is a resposne using js Server.</p>');
     }
+
     else if(req.url=='/about'){
         res.statusCode = 200;
         res.end('<h1> Hello this is about Bhavya.!! </h1> <p>This is about using js Server.</p>');
     }
+
+    else if(req.url=='/hello'){
+        res.statusCode = 200;
+        const data = fs.readFileSync('./index.html');
+        res.end(data.toString());
+    }
+
     else{
         res.statusCode = 200;
         res.end('<h1>Page not found.</h1>')
     }
 })
-
 server.listen({port});
